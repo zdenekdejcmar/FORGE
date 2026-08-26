10a163768930c39ba5711983777aa2f23bff5d1e
Zdeněk Dejčmar <zdenek.dejcmar@gopay.cz>
2026-08-26 13:44:04 +0200

    fix: make eslint config workspace-safe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,15 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
+  js.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
     ignores: ['**/dist/**', '**/node_modules/**'],
+
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.base.json',
         sourceType: 'module',
         ecmaVersion: 'latest',
       },
@@ -17,17 +19,26 @@ export default [
         console: 'readonly',
         window: 'readonly',
         document: 'readonly',
+        process: 'readonly',
       },
     },
+
     plugins: {
       '@typescript-eslint': tseslint,
     },
+
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
+
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
-  js.configs.recommended,
 ];
